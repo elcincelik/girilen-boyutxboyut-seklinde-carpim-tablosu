2f55ce595607ef7d45583aba0b7d6083347fdd09
Elçin Çelik <elcincelik@users.noreply.github.com>
2020-01-14 13:38:39 +0300

    Create carpim.cpp

---
+++ carpim.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+cout<<"Boyut Giriniz."<<endl;
+int boyut;
+cin>>boyut;
+
+	for (int i=1;i<boyut+1;i++){
+		for (int j=1;j<boyut+1;j++){
+			cout<<i*j<<" ";
+		}
+		cout<<endl;
+	}
+}
+	
+	
+	
+	
+	
+	
+	
+	
+
